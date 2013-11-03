442c5ce9d164e29b64eafcd604969be8848a5ead
amurf <ash.d.murphy@gmail.com>
2013-11-03 14:20:05 +1100

    Tidy up scene code.
    Update more condensed, globals removed.
    Drawing code contained in objects for each "thing that needs to be drawn"

--- lib/scenes/testing_scene.dart
+++ lib/scenes/testing_scene.dart
@@ -1,34 +1,16 @@
 part of scenes;
 
 class TestingScene implements Scene {
+  Player player;
 
-  ImageElement img = new ImageElement();
-
-  TestingScene();
+  TestingScene(this.player); // this scene requires a player
 
   GameLoopUpdateFunction onUpdate(GameLoop gameLoop) {
-    img.width  = 100;
-    img.height = 100;
-    img.src    = "spritesheet.png";
-    if ( gameLoop.keyboard.isDown(Keyboard.RIGHT) && gameLoop.gameTime >= gameLoop.last_key_press + 0.200) {
-      if (gameLoop.scale_x == -1 ) {
-        gameLoop.scale_x = -1;
-        gameLoop.x *= 0;
-      }
-      gameLoop.sprite_x += 64;
-      gameLoop.x += 10;
-      gameLoop.scale_x = 1;
-      gameLoop.last_key_press = gameLoop.gameTime;
-    } else if ( gameLoop.keyboard.isDown(Keyboard.LEFT) && gameLoop.gameTime >= gameLoop.last_key_press_2 + 0.200) {
-      if (gameLoop.scale_x == 1 ) {
-        gameLoop.scale_x = -1;
-        gameLoop.x *= -1;
-      }
-      gameLoop.sprite_x += 64;
-      gameLoop.x += 10;
-      gameLoop.last_key_press_2 = gameLoop.gameTime;
+    if ( gameLoop.keyboard.isDown(Keyboard.RIGHT) && player.sprite.readyToAnimate(gameLoop.gameTime)) {
+        player.moveX();
+        player.sprite.animate(gameLoop.gameTime);
     }
-
+    // How to swap scenes on the fly
     if ( gameLoop.frame > 20000 ) {
       gameLoop.changeScene("TestingTwo");
     }
@@ -36,19 +18,11 @@ class TestingScene implements Scene {
 
   GameLoopRenderFunction onRender(GameLoop gameLoop) {
     CanvasRenderingContext context = gameLoop.element.context2D;
-    if (gameLoop.sprite_x == 64*4) {
-      gameLoop.sprite_x = 0;
-    }
-    context.fillStyle = "rgb(200,0,0)";
     context.clearRect(0,0,gameLoop.config['width'],gameLoop.config['height']);
     context.font = "30px sans-serif";
-    //context.fillText("Scene 1", gameLoop.x, gameLoop.last_key_press, 100);
-    // Take the middle 20x20 pixels from the img and stretch them.
-    context.save();
-    context.scale(gameLoop.scale_x , 1);
-    context.drawImageScaledFromSource(img, gameLoop.sprite_x, 0, 64, 64, gameLoop.x, 100, 50, 50);
-    context.restore();
 
+    context.fillText("Scene 1", 0, 100, 100);
+    player.draw(context);
   }
 
 }
