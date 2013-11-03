6357429f8b06598b6f00b55f064da685817bf1b4
amurf <ash.d.murphy@gmail.com>
2013-11-03 14:29:55 +1100

    Change testing_scene_two to use updated testing_scene code

--- lib/scenes/testing_scene_two.dart
+++ lib/scenes/testing_scene_two.dart
@@ -1,18 +1,28 @@
 part of scenes;
 
 class TestingSceneTwo implements Scene {
+  Player player;
 
-  TestingSceneTwo();
+  TestingSceneTwo(this.player); // this scene requires a player obj
 
   GameLoopUpdateFunction onUpdate(GameLoop gameLoop) {
-    gameLoop.y -= gameLoop.dt * 2;
+    if ( gameLoop.keyboard.isDown(Keyboard.RIGHT) && player.readyToAnimate(gameLoop.gameTime)) {
+        player.moveX();
+        player.animate(gameLoop.gameTime);
+    }
+    // How to swap scenes on the fly
+    if ( gameLoop.frame % 211 == 0) {
+      gameLoop.changeScene("Testing");
+    }
   }
 
   GameLoopRenderFunction onRender(GameLoop gameLoop) {
-    CanvasRenderingContext context = gameLoop.element.getContext('2d');
-    context.clearRect(0,0,200,100);
+    CanvasRenderingContext context = gameLoop.element.context2D;
+    context.clearRect(0,0,gameLoop.config['width'],gameLoop.config['height']);
     context.font = "30px sans-serif";
-    context.fillText("Scene 2",gameLoop.x, gameLoop.y, 100);
+
+    context.fillText("Scene 2", 0, 100, 100);
+    player.draw(context);
   }
 
 }
