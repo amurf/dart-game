135ae907b6c929b113247246ed09e3f53ef2a093
amurf <ash.d.murphy@gmail.com>
2013-11-08 15:41:22 +1100

    Change string list to MenuItem list.

--- lib/scenes/main_menu.dart
+++ lib/scenes/main_menu.dart
@@ -3,22 +3,28 @@ part of scenes;
 class MainMenu implements Scene {
   Menu menu;
 
-  MainMenu() {
-    this.menu = new Menu(["Start", "Options", "Exit"], 50);
+  MainMenu(GameLoop gameLoop) {
+    this.menu = new Menu([
+        new MenuItem("Start",   onClick: start, onMouseOver: startDown),
+        new MenuItem("Options", onClick: (gameLoop) => print("OPTIONZ")),
+        new MenuItem("Exit",    onClick: (gameLoop) => print("EXIT!")),
+    ]);
+    this.menu.onClickListener(gameLoop);
   }
 
-  GameLoopUpdateFunction onUpdate(GameLoop gameLoop) {
-    var clientRect = gameLoop.element.getBoundingClientRect();
-    gameLoop.element.onClick.listen((e) {
-      gameLoop.changeScene("TestingSceneTwo");
-    });
-  }
+  GameLoopUpdateFunction onUpdate(GameLoop gameLoop) { }
 
   GameLoopRenderFunction onRender(GameLoop gameLoop) {
     CanvasRenderingContext context = gameLoop.element.context2D;
-
     context.clearRect(0,0,gameLoop.config['width'],gameLoop.config['height']);
     menu.draw(context);
   }
 
+  Function start(GameLoop gameLoop) {
+    gameLoop.changeScene("TestingSceneTwo");
+  }
+
+  Function startDown(GameLoop gameLoop) {
+    this.menu.items[0].color = "red";
+  }
 }
