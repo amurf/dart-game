118199081b7891d450dfefc4ca9ad3d527e0b8f4
amurf <ash.d.murphy@gmail.com>
2013-11-08 15:41:32 +1100

    Move to using MenuItem and add proper onClick callbacks.

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,19 +1,51 @@
+import 'menu_item.dart';
+import 'dart:math';
+
 class Menu {
-  List items;
-  num padding;
-  
-  Menu(this.items, this.padding);
-  
+  List<MenuItem> items;
+  num itemPadding = 40;
+  num fontSize    = 20;
+  num width       = 500;
+
+  Menu(this.items, {this.itemPadding, this.fontSize, this.width});
+
+  void onClickListener(GameLoop gameLoop) {
+      gameLoop.element.onClick.listen((e) {
+        var rect        = gameLoop.element.getBoundingClientRect();
+        var clickedPoint = new Point(e.clientX - rect.left, e.clientY - rect.top);
+        this.clickCheck(clickedPoint, gameLoop);
+      });
+  }
+
 
   void draw(CanvasRenderingContext context) {
-    context.font = "30px sans-serif";
+    context.font = fontSize.toString()+"pt sans-serif";
     for (num x = 0; x < items.length; x++) {
-      context.fillText(items[x], 500, padding*(x+1), 100);
+      context.save();
+      context.fillStyle = items[x].color;
+      context.fillText(items[x].label, width, itemPadding * (x+1) + fontSize);
+      context.restore();
+    }
+
+  }
+
+  void clickCheck(Point clickedPoint, GameLoop gameLoop) {
+    for (num x = 0; x < items.length; x++) {
+      var stringWidth = stringWidth(items[x].label, gameLoop);
+      var stringRect  = new Rectangle(this.width, itemPadding * (x+1), stringWidth, fontSize);
+      if (detectCollision(stringRect, clickedPoint)) {
+        items[x].onClick(gameLoop);
+      }
     }
   }
 
-  void checkClick(num x, num y) {
-    print(x);
-  }
+  num stringWidth(String str, GameLoop gameLoop) => gameLoop.element.context2D.measureText(str).width;
+
+  bool detectCollision(Rectangle rect, Point mouseClick) =>
+    ( mouseClick.x > rect.left && mouseClick.x < rect.right && mouseClick.y >= rect.top && mouseClick.y <= rect.bottom );
+
 
 }
+
+
+
