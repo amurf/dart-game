6053efc97a4ef8b7c7c61e7a6cf8a89afecd6a1c
amurf <ash.d.murphy@gmail.com>
2013-11-12 16:47:33 +1100

    Remove 'this' as per dartlang style guidelines Test keys to swap block forms. #TEMPORARY#

--- lib/keymap.dart
+++ lib/keymap.dart
@@ -7,28 +7,46 @@ class KeyMap {
   Player player;
 
   KeyMap(this.player) {
-    this.keyMap = new Map();
-   
+    keyMap = new Map();
+
     // Adding functions to keycodes :)
     // (code, delay, function)
     addKey(Keyboard.RIGHT, 0, (gameLoop, player) {
         player.moveX(gameLoop);
     });
-    
+
     addKey(Keyboard.LEFT, 0, (gameLoop, player) {
         player.moonwalk(gameLoop);
     });
-    
+
     addKey(Keyboard.M, 0, (gameLoop, player) {
         gameLoop.changeScene("MainMenu");
     });
+
+    addKey(Keyboard.I, 0, (gameLoop, player) {
+        gameLoop.sceneManager.currentScene.blocks[0].image.src = "images/ice.png";
+        gameLoop.sceneManager.currentScene.blocks[0].momentum = 0;
+        gameLoop.sceneManager.currentScene.blocks[1].image.src = "images/ice.png";
+        gameLoop.sceneManager.currentScene.blocks[1].momentum = 0;
+        gameLoop.sceneManager.currentScene.blocks[2].image.src = "images/ice.png";
+        gameLoop.sceneManager.currentScene.blocks[2].momentum = 0;
+    });
+
+    addKey(Keyboard.W, 0, (gameLoop, player) {
+        gameLoop.sceneManager.currentScene.blocks[0].image.src = "images/water.png";
+        gameLoop.sceneManager.currentScene.blocks[0].momentum = 0.5;
+        gameLoop.sceneManager.currentScene.blocks[1].image.src = "images/water.png";
+        gameLoop.sceneManager.currentScene.blocks[1].momentum = 0.5;
+        gameLoop.sceneManager.currentScene.blocks[2].image.src = "images/water.png";
+        gameLoop.sceneManager.currentScene.blocks[2].momentum = 0.5;
+    });
   }
   void addKey(num keyCode, num keyDelay, Function keyFunction) {
-    this.keyMap[keyCode] = new KeyHandler(keyCode, keyDelay, keyFunction);
+    keyMap[keyCode] = new KeyHandler(keyCode, keyDelay, keyFunction);
   }
-  
+
   void checkKeyStatus(GameLoopHtml gameLoop) {
-    this.keyMap.forEach((key, val) {
+    keyMap.forEach((key, val) {
       if (isKeyDown(key, gameLoop)) {
         val.doKeyFunction(gameLoop, player);
       }
