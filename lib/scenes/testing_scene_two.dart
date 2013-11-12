0e253d63c32614e51456df8a2bb30f9c0897a31a
amurf <ash.d.murphy@gmail.com>
2013-11-12 16:49:30 +1100

    Remove 'this' as per dartlang style guidelines Add temporary block list for testing

--- lib/scenes/testing_scene_two.dart
+++ lib/scenes/testing_scene_two.dart
@@ -2,24 +2,35 @@ part of scenes;
 
 class TestingSceneTwo implements Scene {
   Player player;
+  List<Block> blocks;
 
-  TestingSceneTwo(this.player); // this scene requires a player obj
+  // this scene requires a player obj
+  TestingSceneTwo(this.player) {
+    blocks = new List();
+    blocks.add(new Block(x: 100, y: 0, width: 50, height: 10));
+    blocks.add(new Block(x: 150, y: 0, width: 50, height: 15));
+    blocks.add(new Block(x: 200, y: 0, width: 50, height: 40));
+  }
 
   GameUpdateFunction onUpdate(Game gameLoop) {
+    for ( Block block in blocks ) {
+      if ( block.collidable && player.x >= (block.x - block.width)
+          && ( player.y - (block.y + block.height) <= 0 )
+          ) {
+        player.momentum = block.momentum;
+      }
+    }
+
     player.checkKeyStatus(gameLoop);
-    // How to swap scenes on the fly
-    //if ( gameLoop.frame % 471 == 0 ) {
-    //  gameLoop.changeScene("TestingTwo");
-    //}
   }
 
   GameRenderFunction onRender(Game gameLoop) {
     CanvasRenderingContext2D context = gameLoop.element.context2D;
     context.clearRect(0,0,gameLoop.config['width'],gameLoop.config['height']);
-    context.font = "30px sans-serif";
-
-    context.fillText("Scene 1", 0, 100, 100);
     player.draw(context);
+    for ( Block block in blocks ) {
+      block.draw(context);
+    }
   }
 
 }
