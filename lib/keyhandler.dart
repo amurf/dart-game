8ca031b58e96baed7ff3e9c2dd12225d3cc93a44
amurf <ash.d.murphy@gmail.com>
2013-11-12 16:47:01 +1100

    Remove 'this' as per dartlang style guidelines

--- lib/keyhandler.dart
+++ lib/keyhandler.dart
@@ -10,9 +10,9 @@ class KeyHandler {
   KeyHandler(this.keyCode, this.keyDelay, this.keyFunction);
 
   void doKeyFunction(Game gameLoop, Player player) {
-    if (this.isReady(gameLoop))
-      this.keyFunction(gameLoop, player);
-      this.lastPressed = gameLoop.gameTime;
+    if (isReady(gameLoop))
+      keyFunction(gameLoop, player);
+      lastPressed = gameLoop.gameTime;
   }
 
   bool isReady(Game gameLoop) => gameLoop.gameTime > keyDelay + lastPressed;
